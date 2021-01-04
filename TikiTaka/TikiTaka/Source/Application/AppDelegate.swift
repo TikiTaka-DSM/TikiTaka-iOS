91ccbbc5b7b19b3518cdb0ee17c566e96973e660
Ga-yo <39812@naver.com>
2021-01-04 16:42:06 +0900

    [Remove] SceneDelegate

---
+++ TikiTaka/TikiTaka/Source/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TikiTaka
+//
+//  Created by 이가영 on 2020/12/14.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
