29e7329174a4e931654e9614a6e8c31a689ca752
Ga-yo <39812@naver.com>
2021-01-06 09:43:46 +0900

    [Feat] Extensions

---
+++ TikiTaka/TikiTaka/Source/Extensions/UIViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UIViewController.swift
+//  TikiTaka
+//
+//  Created by 이가영 on 2021/01/05.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func goNext(_ identifier: String) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: identifier)
+        navigationController?.pushViewController(vc!, animated: true)
+    }
+    
+    func setAlert(_ titlt: String) {
+        let alert = UIAlertController(title: "알람", message: title, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(action)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension UIButton {
+    open override var isEnabled: Bool {
+        didSet {
+            print("didset \(isEnabled)")
+        }
+        willSet{
+            print("willSet \(isEnabled)")
+        }
+    }
+}
