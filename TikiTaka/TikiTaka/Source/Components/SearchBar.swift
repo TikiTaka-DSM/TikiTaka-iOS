e35ce9e293f0b05bcf62e555e22388d0ed229587
Ga-yo <39812@naver.com>
2021-01-06 09:42:41 +0900

    [Feat] Custom Search Bar

--- TikiTaka/TikiTaka/Source/Components/SearchBar.swift
+++ TikiTaka/TikiTaka/Source/Components/SearchBar.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import UIKit
+
+class SearchBar: UIView {
+    
+    private let searchTextField = UITextField().then {
+        $0.placeholder = "Search"
+        $0.textColor = .white
+        $0.backgroundColor = .clear
+    }
+    
+    private let doneBtn = UIButton().then {
+        $0.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        $0.tintColor = .white
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.cornerRadius = 28
+        self.backgroundColor = PointColor.primary
+        
+        self.addSubview(searchTextField)
+        self.addSubview(doneBtn)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func updateConstraints() {
+        
+        searchTextField.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalTo(self.snp.leading).offset(20)
+            $0.center.equalToSuperview()
+        }
+        
+        doneBtn.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-10)
+            $0.centerY.equalToSuperview()
+        }
+        
+        super.updateConstraints()
+    }
+    
+}
