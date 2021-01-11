6bea69df15826cdff0729ea84c9a55b51184d1df
Ga-yo <39812@naver.com>
2021-01-11 23:19:40 +0900

    [Feat] ChatTableViewCell
    custom tableView cell

---
+++ TikiTaka/TikiTaka/Source/Components/ChatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatTableViewCell.swift
+//  TikiTaka
+//
+//  Created by 이가영 on 2021/01/11.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
