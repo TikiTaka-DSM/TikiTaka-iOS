d9a80ec491fcb83c353b9193a29459d1c7453fc7
Ga-yo <39812@naver.com>
2021-01-12 01:15:57 +0900

    [Feat] Chat Model

---
+++ TikiTaka/TikiTaka/Source/Models/Chat.swift
@@ -0,0 +1,26 @@
+//
+//  Chat.swift
+//  TikiTaka
+//
+//  Created by 이가영 on 2021/01/12.
+//
+
+import Foundation
+
+struct Message: Codable {
+    let user: [User]
+    let message: String
+    let photo: String
+    let voice: String
+    let createdAt: String
+}
+
+struct User: Codable {
+    let name: String
+    let img: String
+}
+
+struct MessageData: Codable {
+    let roomData: RoomInfo
+    let messageData: [Message]
+}
