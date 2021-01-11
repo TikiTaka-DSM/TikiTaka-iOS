f41abe51e04a5ae9b0664f684c6b2fd4a6f1796f
Ga-yo <39812@naver.com>
2021-01-11 23:20:12 +0900

    [Add] Cell Layout

--- TikiTaka/TikiTaka/Source/Components/ChatTableViewCell.swift
+++ TikiTaka/TikiTaka/Source/Components/ChatTableViewCell.swift
@@ -9,9 +9,49 @@ import UIKit
 
 class ChatTableViewCell: UITableViewCell {
 
+    let chatImg = UIImageView()
+    let chatName = UILabel()
+    let chatTime = UILabel()
+    let lastMessage = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(chatImg)
+        contentView.addSubview(chatName)
+        contentView.addSubview(chatTime)
+        contentView.addSubview(lastMessage)
+        
+        chatImg.snp.makeConstraints {
+            $0.top.equalTo(contentView.snp.top).offset(9)
+            $0.bottom.equalTo(contentView.snp.bottom).offset(-8)
+            $0.width.height.equalTo(53)
+            $0.leading.equalTo(contentView.snp.leading).offset(5)
+        }
+        
+        chatName.snp.makeConstraints {
+            $0.leading.equalTo(chatImg.snp.trailing).offset(18)
+            $0.top.equalTo(chatImg.snp.top)
+        }
+        
+        lastMessage.snp.makeConstraints {
+            $0.leading.equalTo(chatImg.snp.trailing).offset(18)
+            $0.top.equalTo(chatName.snp.bottom).offset(5)
+        }
+        
+        chatTime.snp.makeConstraints {
+            $0.trailing.equalTo(contentView.snp.trailing).offset(-8)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +59,5 @@ class ChatTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
 }
