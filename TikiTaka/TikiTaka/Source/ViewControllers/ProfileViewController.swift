1f318f75263592226a30fa466b39f7c65daaf3f2
Ga-yo <39812@naver.com>
2021-01-12 17:57:47 +0900

    [Add] BottomBtn Layout

--- TikiTaka/TikiTaka/Source/ViewControllers/ProfileViewController.swift
+++ TikiTaka/TikiTaka/Source/ViewControllers/ProfileViewController.swift
@@ -21,6 +21,25 @@ class ProfileViewController: UIViewController {
         $0.text = "요즘 DPR 노래가 좋더라~!"
     }
     
+    private let chatBtn = UIButton().then {
+        $0.setTitle("채팅하기", for: .normal)
+        $0.backgroundColor = PointColor.primary
+        $0.setTitleColor(.white, for: .normal)
+    }
+    
+    private let blockBtn = UIButton().then {
+        $0.setTitle("차단", for: .normal)
+        $0.backgroundColor = PointColor.primary
+        $0.setTitleColor(.white, for: .normal)
+    }
+    
+    private let addBtn = UIButton().then {
+        $0.setTitle("친구 추가", for: .normal)
+        $0.backgroundColor = PointColor.primary
+        $0.setTitleColor(.white, for: .normal)
+    }
+    private var isFriend: Bool = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,14 +49,25 @@ class ProfileViewController: UIViewController {
         view.addSubview(userImageView)
         view.addSubview(userNameLabel)
         view.addSubview(statusLabel)
+        view.addSubview(chatBtn)
+        view.addSubview(blockBtn)
+        view.addSubview(addBtn)
         
         setUpConstraint()
     }
     
-    func setUpConstraint() {
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
+        statusLabel.underLine()
         self.forCornerRadius(userImageView)
-        
+        self.forCornerRadius(chatBtn)
+        self.forCornerRadius(blockBtn)
+        self.forCornerRadius(addBtn)
+    }
+    
+    func setUpConstraint() {
+   
         userImageView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(self.view.snp.top).offset(75)
@@ -54,6 +84,32 @@ class ProfileViewController: UIViewController {
             $0.top.equalTo(userNameLabel.snp.bottom).offset(16)
         }
         
+        if isFriend {
+            addBtn.isHidden = true
+            
+            chatBtn.snp.makeConstraints {
+                $0.height.width.equalTo(90)
+                $0.leading.equalToSuperview().offset(75)
+                $0.top.equalTo(statusLabel.snp.bottom).offset(120)
+            }
+            
+            blockBtn.snp.makeConstraints {
+                $0.height.width.equalTo(90)
+                $0.trailing.equalToSuperview().offset(-75)
+                $0.top.equalTo(statusLabel.snp.bottom).offset(120)
+            }
+            
+        } else {
+            chatBtn.isHidden = true
+            blockBtn.isHidden = true
+            
+            
+            addBtn.snp.makeConstraints {
+                $0.height.width.equalTo(105)
+                $0.top.equalTo(statusLabel.snp.bottom).offset(120)
+                $0.centerX.equalToSuperview()
+            }
+        }
     }
     
     /*
