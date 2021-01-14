636da37c9147b34f6a971e33cbe232cf371f3d7d
Ga-yo <39812@naver.com>
2021-01-14 23:50:12 +0900

    [Add] Bind Friend List

--- TikiTaka/TikiTaka/Source/ViewControllers/FriendViewController.swift
+++ TikiTaka/TikiTaka/Source/ViewControllers/FriendViewController.swift
@@ -6,18 +6,24 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class FriendViewController: UIViewController {
 
-    let friendsTableView = UITableView().then {
+    private let friendsTableView = UITableView().then {
         $0.separatorColor = PointColor.primary
         $0.rowHeight = 73
+    }
+    
+    private let searchBar = SearchBar().then {
         $0.backgroundColor = PointColor.primary
     }
     
-    let searchBar = SearchBar().then {
-        $0.backgroundColor = PointColor.primary
-    }
+    private let dummy = BehaviorRelay<[Friend]>(value: [])
+    private let disposeBag = DisposeBag()
+    private let loadData = BehaviorRelay<Void>(value: ())
+    private let viewModel = FriendViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +31,10 @@ class FriendViewController: UIViewController {
         view.addSubview(friendsTableView)
         view.addSubview(searchBar)
         
+
+        dummy.accept([Friend(id: "asdf", img: nil, name: "gi", statusMessage: "Cjdq"), Friend(id: "asdf", img: nil, name: "gi", statusMessage: "Cjdq")])
+        
+        setTableView()
     }
     
     override func viewDidLayoutSubviews() {
@@ -33,13 +43,27 @@ class FriendViewController: UIViewController {
         setUpConstraint()
     }
     
+    func bindViewModel() {
+        let input = FriendViewModel.Input(loadFriends: loadData.asSignal(onErrorJustReturn: ()))
+        let output = viewModel.transform(input: input)
+        
+        output.loadData.drive(friendsTableView.rx.items(cellIdentifier: "friendsCell", cellType: UITableViewCell.self)) { row, model, cell in
+            cell.textLabel?.text = model.name
+            cell.imageView?.kf.setImage(with: URL(string: model.img))
+        }.disposed(by: disposeBag)
+    }
+    
     func setTableView() {
         friendsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "friendsCell")
         
+        dummy.bind(to: friendsTableView.rx.items(cellIdentifier: "friendsCell")) { row, model, cell in
+            cell.imageView?.image = UIImage(named: "TikiTaka_logo.png")
+            self.forCornerRadius(cell.imageView!)
+            cell.textLabel?.text = model.name
+        }.disposed(by: disposeBag)
     }
     
     func setUpConstraint() {
-
         searchBar.snp.makeConstraints {
             $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
             $0.leading.equalTo(self.view)
@@ -48,10 +72,10 @@ class FriendViewController: UIViewController {
         }
         
         friendsTableView.snp.makeConstraints {
-            $0.top.equalTo(searchBar.snp.bottom)
+            $0.top.equalTo(searchBar.snp.bottom).offset(40)
             $0.leading.equalTo(view.snp.leading)
             $0.trailing.equalTo(view.snp.trailing)
+            $0.bottom.equalTo(view.snp.bottom)
         }
-        
     }
 }
