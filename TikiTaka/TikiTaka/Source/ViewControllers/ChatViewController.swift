9c10d2d9b56085bb837e0d9a559af7975fd559cf
Ga-yo <39812@naver.com>
2021-01-14 23:33:52 +0900

    [Feat] ChatVC

---
+++ TikiTaka/TikiTaka/Source/ViewControllers/ChatViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ChatViewController.swift
+//  TikiTaka
+//
+//  Created by 이가영 on 2021/01/12.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import MessageKit
+
+class ChatViewController: MessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
