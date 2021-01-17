deb19e400123496cff88cff18d49150f8d1c4e22
Ga-yo <39812@naver.com>
2021-01-17 23:34:53 +0900

    [Feat] ChatInputField

---
+++ TikiTaka/TikiTaka/Source/Components/ChatInputField.swift
@@ -0,0 +1,20 @@
+//
+//  ChatInputField.swift
+//  TikiTaka
+//
+//  Created by 이가영 on 2021/01/17.
+//
+
+import UIKit
+
+class ChatInputField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
