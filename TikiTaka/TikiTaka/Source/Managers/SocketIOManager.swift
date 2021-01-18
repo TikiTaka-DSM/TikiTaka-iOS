3b663e2e5495f71319a840598116cea54a0dbd89
Ga-yo <39812@naver.com>
2021-01-18 18:18:59 +0900

    [Modify] Socket Default Setting

--- TikiTaka/TikiTaka/Source/Managers/SocketIOManager.swift
+++ TikiTaka/TikiTaka/Source/Managers/SocketIOManager.swift
@@ -8,8 +8,38 @@
 import Foundation
 import SocketIO
 
-class socket: NSObject {
-    static let shared = socket()
+class SocketIOManager: NSObject {
     
-    var manager = SocketManager(socketURL: URL(string: ""), config: [.log(false, .compress)])
+    static let shared = SocketIOManager()
+    
+    var manager = SocketManager(socketURL: URL(string: "")!, config: [.log(false), .compress])
+    var socket: SocketIOClient!
+
+    override init() {
+        super.init()
+        
+        socket = self.manager.defaultSocket
+    }
+    
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func sendMessage(roomId: String, message: String, token: String) -> [String : Any] {
+        return ["roomId" : roomId, "message" : message , "token" : token ]
+    }
+    
+    func sendImage(roomId: String, image: Data, token: String) -> [String: Any] {
+        return ["roomId": roomId, "image": image, "token": token]
+    }
+    
+    func sendAudio(roomId: String, audio: Data, token: String) -> [String: Any] {
+        return ["roomId": roomId, "audio": audio, "token": token]
+    }
+    
+
 }
