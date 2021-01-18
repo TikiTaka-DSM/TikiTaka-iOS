6056f4318f52f6fd945c750c994e78a85894b606
Ga-yo <39812@naver.com>
2021-01-18 12:49:49 +0900

    [Add] ChatInputField Layout

--- TikiTaka/TikiTaka/Source/Components/ChatInputField.swift
+++ TikiTaka/TikiTaka/Source/Components/ChatInputField.swift
@@ -7,14 +7,84 @@
 
 import UIKit
 
-class ChatInputField: UITextField {
+class ChatInputField: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let chatImg = UIButton().then {
+        $0.setImage(UIImage(systemName: "mic.fill"), for: .normal)
+        $0.tintColor = .white
     }
-    */
-
+    
+    let chatAudio = UIButton().then {
+        $0.setImage(UIImage(systemName: "photo"), for: .normal)
+        $0.tintColor = .white
+    }
+    
+    let inputTextField = UITextField().then {
+        $0.backgroundColor = .white
+        $0.addLeftPadding()
+    }
+    
+    let sendBtn = UIButton().then {
+        $0.setTitle("전송", for: .normal)
+    }
+    
+    let stackView = UIStackView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.alignment = .fill
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        inputTextField.layer.cornerRadius = 20
+        
+        self.backgroundColor = PointColor.primary
+        
+        self.addSubview(stackView)
+        
+        stackView.addArrangedSubview(chatImg)
+        stackView.addArrangedSubview(chatAudio)
+        stackView.addArrangedSubview(inputTextField)
+        stackView.addArrangedSubview(sendBtn)
+        
+        sendBtn.isHidden = true
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func updateConstraints() {
+        
+        chatImg.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(26)
+        }
+        
+        chatAudio.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(26)
+        }
+        
+        inputTextField.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(26)
+        }
+        
+        sendBtn.snp.makeConstraints {
+            $0.trailing.equalTo(self.snp.trailing).offset(-10)
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(26)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.leading.equalTo(self.snp.leading).offset(10)
+            $0.trailing.equalTo(self.snp.trailing).offset(-10)
+            $0.height.equalTo(26)
+            $0.centerY.equalToSuperview()
+        }
+        
+        super.updateConstraints()
+    }
 }
