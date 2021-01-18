34c83eb38c327d7ee6da02d0a84f7e0f9b26abb1
Ga-yo <39812@naver.com>
2021-01-18 14:26:06 +0900

    [Add] ChatVC InputBar Layout

--- TikiTaka/TikiTaka/Source/ViewControllers/ChatViewController.swift
+++ TikiTaka/TikiTaka/Source/ViewControllers/ChatViewController.swift
@@ -9,24 +9,64 @@ import UIKit
 import RxSwift
 import RxCocoa
 import MessageKit
+import InputBarAccessoryView
 
-class ChatViewController: MessagesViewController {
+class ChatViewController: UIViewController {
 
+    private let chatTableView = UITableView()
+    private let inputBar = ChatInputField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+        view.addSubview(chatTableView)
+        view.addSubview(inputBar)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear(note:)), name: UIResponder.keyboardWillShowNotification , object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear(note:)), name: UIResponder.keyboardWillHideNotification , object: nil)
+        
+        inputBar.inputTextField.delegate = self
+        setUpConstraint()
+    }
+      
+    override func viewWillAppear(_ animated: Bool) {
+        chatTableView.separatorColor = .clear
+        chatTableView.separatorInset = .zero
+        chatTableView.separatorStyle = .none
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpConstraint() {
+        chatTableView.snp.makeConstraints {
+            $0.top.equalTo(view.snp.top)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        inputBar.snp.makeConstraints {
+            $0.top.equalTo(chatTableView.snp.bottom)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            $0.height.equalTo(40)
+        }
     }
-    */
+    
+    @objc func keyboardWillAppear(note: Notification){
+        if let keyboardSize = (note.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            self.view.frame.origin.y -= keyboardSize.height
+            inputBar.sendBtn.isHidden = false
+        }
+        
+    }
+    
+    @objc func keyboardWillDisappear(note: Notification){
+        self.view.frame.origin.y = 0
+        inputBar.sendBtn.isHidden = true
+    }
+}
 
+extension ChatViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+    }
 }
