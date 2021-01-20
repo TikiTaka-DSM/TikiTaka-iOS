73e467a2ce3c4f5b3a67a1b7a18408c71565897f
Ga-yo <39812@naver.com>
2021-01-20 16:11:07 +0900

    [Add] Friend Select Event

--- TikiTaka/TikiTaka/Source/ViewModels/FriendViewModel.swift
+++ TikiTaka/TikiTaka/Source/ViewModels/FriendViewModel.swift
@@ -14,19 +14,22 @@ class FriendViewModel: ViewModelType {
     
     struct Input {
         let loadFriends: Signal<Void>
+        let selectFriend: Signal<IndexPath>
         let searchName: Signal<String>
         let searchFriend: Signal<Void>
     }
     
     struct Output {
-        let loadData: Driver<[Friend]>
-        let searchData: Driver<SearchUser?>
+        let loadData: BehaviorRelay<[Friend]>
+        let searchData: Driver<[SearchUser]?>
+        let selectData: Driver<String>
     }
     
     func transform(input: Input) -> Output {
         let api = Service()
         let loadData = BehaviorRelay<[Friend]>(value: [])
-        let searchData = BehaviorRelay<SearchUser?>(value: nil)
+        let searchData = BehaviorRelay<[SearchUser]?>(value: nil)
+        let selectData = PublishRelay<String>()
         
         input.loadFriends.asObservable().subscribe(onNext: { _ in
             api.getFriends().subscribe(onNext: { data, response in
@@ -34,9 +37,15 @@ class FriendViewModel: ViewModelType {
                 case .success:
                     loadData.accept(data!.friends)
                 default:
-                    print("df")
+                    print("친구목록이 없습니다.")
+                    loadData.accept([])
                 }
             }).disposed(by: self.disposeBag)
+        }).disposed(by: disposeBag)
+        
+        input.selectFriend.asObservable().subscribe(onNext: { indexPath in
+            let friend = loadData.value
+            selectData.accept(friend[indexPath.row].id)
         }).disposed(by: disposeBag)
         
         input.searchFriend.asObservable().withLatestFrom(input.searchName).subscribe(onNext: { name in
@@ -50,7 +59,7 @@ class FriendViewModel: ViewModelType {
             }).disposed(by: self.disposeBag)
         }).disposed(by: disposeBag)
         
-        return Output(loadData: loadData.asDriver(), searchData: searchData.asDriver())
+        return Output(loadData: loadData, searchData: searchData.asDriver(), selectData: selectData.asDriver(onErrorJustReturn: ""))
     }
 
 
