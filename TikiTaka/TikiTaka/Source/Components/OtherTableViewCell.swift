6042f84758a415b00b78bfd5e661b63b81c0e22c
Ga-yo <39812@naver.com>
2021-01-23 22:43:21 +0900

    [Feat] OtherTableViewCell

---
+++ TikiTaka/TikiTaka/Source/Components/OtherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OtherTableViewCell.swift
+//  TikiTaka
+//
+//  Created by 이가영 on 2021/01/23.
+//
+
+import UIKit
+
+class OtherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
