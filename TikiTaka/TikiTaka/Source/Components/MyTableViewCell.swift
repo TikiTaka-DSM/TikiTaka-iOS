39ef1d650058dbc3497e8faaebb9cf34283e54c7
Ga-yo <39812@naver.com>
2021-01-24 20:17:39 +0900

    [Add] Only Text Chat UI
    내 시점

--- TikiTaka/TikiTaka/Source/Components/MyTableViewCell.swift
+++ TikiTaka/TikiTaka/Source/Components/MyTableViewCell.swift
@@ -9,6 +9,44 @@ import UIKit
 
 class MyTableViewCell: UITableViewCell {
 
+    let messageLabel = UILabel().then {
+        $0.textColor = .black
+        $0.numberOfLines = 0
+    }
+        
+    let bubbleView = UIImageView().then {
+        $0.backgroundColor = PointColor.primary
+        $0.layer.cornerRadius = 10
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.addSubview(bubbleView)
+        self.addSubview(messageLabel)
+        
+        messageLabel.numberOfLines = 0
+        
+        messageLabel.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top).offset(16)
+            $0.bottom.equalTo(self.snp.bottom).offset(-16)
+            $0.trailing.equalTo(self.snp.trailing).offset(-11)
+            $0.width.lessThanOrEqualTo(250)
+        }
+        
+        bubbleView.snp.makeConstraints {
+            $0.top.equalTo(messageLabel.snp.top).offset(-8)
+            $0.bottom.equalTo(messageLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(messageLabel.snp.leading).offset(-8)
+            $0.trailing.equalTo(messageLabel.snp.trailing).offset(8)
+        }
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -17,7 +55,6 @@ class MyTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
