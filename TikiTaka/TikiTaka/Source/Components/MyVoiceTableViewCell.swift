b71cb5f030379d6fe3772d2a841a6ec81373394f
Ga-yo <39812@naver.com>
2021-01-25 00:04:03 +0900

    [Feat] MyVoiceTableViewCell

---
+++ TikiTaka/TikiTaka/Source/Components/MyVoiceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyVoiceTableViewCell.swift
+//  TikiTaka
+//
+//  Created by 이가영 on 2021/01/24.
+//
+
+import UIKit
+
+class MyVoiceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
