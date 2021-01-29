c60ad09c915a47c93403a37f402cc9b085299e80
Ga-yo <39812@naver.com>
2021-01-29 18:24:30 +0900

    [Feat] FriendTableViewCell

---
+++ TikiTaka/TikiTaka/Source/Components/FriendTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FriendTableViewCell.swift
+//  TikiTaka
+//
+//  Created by 이가영 on 2021/01/29.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
