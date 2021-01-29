7a2f89c35778de8f00f5a649bbd270206adc8f78
Ga-yo <39812@naver.com>
2021-01-29 17:13:24 +0900

    [Add] ChatListViewModel

--- TikiTaka/TikiTaka/Source/ViewModels/MainViewModel.swift
+++ TikiTaka/TikiTaka/Source/ViewModels/MainViewModel.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class MainViewModel: ViewModelType {
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+        let loadChatList: Signal<Void>
+        let selectRoom: Signal<IndexPath>
+    }
+    
+    struct Output {
+        let loadData: Driver<[ChatListSection]>
+        let selectData: Driver<Int>
+    }
+    
+    func transform(input: Input) -> Output {
+        let api = Service()
+        let loadData = BehaviorRelay<[ChatListSection]>(value: [])
+        let selectData = PublishRelay<Int>()
+        
+        input.loadChatList.asObservable().subscribe(onNext: { _ in
+            api.getChatList().subscribe(onNext: { data, response in
+                print(response)
+                switch response {
+                case .success:
+                    var result = [String : [ChatList]]()
+                    for i in 0..<data!.count {
+                        if result[String(i)] == nil {
+                            result[String(i)] = data!
+                            continue
+                        }
+                        result[String(i)]?.append(data![i])
+                    }
+                    
+                    loadData.accept(result.compactMap { ChatListSection(model: $0.key, items: $0.value) })
+                    print(result)
+                default:
+                    print("asdf")
+                }
+            }).disposed(by: self.disposeBag)
+        }).disposed(by: disposeBag)
+        
+        input.selectRoom.asObservable().subscribe(onNext: { indexPath in
+            let room = loadData.value
+            selectData.accept(room[indexPath.row].items[indexPath.row].roomId)
+        }).disposed(by: disposeBag)
+
+        return Output(loadData: loadData.asDriver(), selectData: selectData.asDriver(onErrorJustReturn: 0))
+    }
+
+
+}
