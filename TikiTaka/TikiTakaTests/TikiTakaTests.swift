3c37522364ae21f29476301714758d59758fcd8a
Ga-yo <39812@naver.com>
2021-02-06 20:55:24 +0900

    [Feat] Test

--- TikiTaka/TikiTakaTests/TikiTakaTests.swift
+++ TikiTaka/TikiTakaTests/TikiTakaTests.swift
@@ -6,11 +6,14 @@
 //
 
 import XCTest
+@testable import TikiTaka
 
 class TikiTakaTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        //        var result = ""
     }
 
     override func tearDownWithError() throws {
