89620344896f42f06afcacb9c963f75f523f1836
Ga-yo <39812@naver.com>
2021-02-10 12:50:33 +0900

    [Add] socket disconnect when applicationWillTerminate

--- TikiTaka/TikiTaka/Source/Application/AppDelegate.swift
+++ TikiTaka/TikiTaka/Source/Application/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-    
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        SocketIOManager.shared.socket.disconnect()
+    }
 }
 
