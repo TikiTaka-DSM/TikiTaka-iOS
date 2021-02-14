a60146b5346a7d8eff74e519c2f321e720c4a6f4
Ga-yo <39812@naver.com>
2021-02-15 01:34:39 +0900

    [Add] token check for root view

--- TikiTaka/TikiTaka/Source/Application/AppDelegate.swift
+++ TikiTaka/TikiTaka/Source/Application/AppDelegate.swift
@@ -13,11 +13,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+
+        if TokenManager.currentToken?.tokens.accessToken != nil {
+            let viewController = storyboard.instantiateViewController(withIdentifier: "Main")
+
+            self.window?.rootViewController = viewController
+            self.window?.makeKeyAndVisible()
+        }else {
+            let viewController = storyboard.instantiateViewController(withIdentifier: "SignIn") as! SignInViewController
+
+            self.window?.rootViewController = viewController
+            self.window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         SocketIOManager.shared.socket.disconnect()
     }
+    
 }
 
