b804806dcbfe31d5a083bc3fc88f9500d98cec59
Ga-yo <39812@naver.com>
2021-03-07 22:39:36 +0900

    [Add] fix inputTextField setting

--- TikiTaka/TikiTaka/Source/Components/ChatInputField.swift
+++ TikiTaka/TikiTaka/Source/Components/ChatInputField.swift
@@ -21,17 +21,18 @@ class ChatInputField: UIView {
     
     let inputTextField = UITextView().then {
         $0.backgroundColor = .white
-        $0.font = .monospacedSystemFont(ofSize: 13, weight: .regular)
-        $0.returnKeyType = .done
+        $0.font = .monospacedSystemFont(ofSize: 16, weight: .regular)
+        $0.returnKeyType = .default
+        $0.keyboardDismissMode = .interactive
         $0.textContainerInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
     }
     
     let sendBtn = UIButton().then {
         $0.setTitle("전송", for: .normal)
+        $0.isHidden = false
     }
     
-    private let stackView = UIStackView().then {
-        $0.translatesAutoresizingMaskIntoConstraints = false
+    let stackView = UIStackView().then {
         $0.axis = .horizontal
         $0.spacing = 10
         $0.alignment = .fill
@@ -41,7 +42,7 @@ class ChatInputField: UIView {
         $0.textColor = .white
         $0.font = .monospacedSystemFont(ofSize: 10, weight: .regular)
     }
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         inputTextField.layer.cornerRadius = 16
@@ -65,7 +66,7 @@ class ChatInputField: UIView {
     }
     
     override func updateConstraints() {
-        
+
         chatImg.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.width.height.equalTo(26)
@@ -79,18 +80,21 @@ class ChatInputField: UIView {
         inputTextField.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.height.equalTo(26)
+            $0.height.greaterThanOrEqualTo(50)
         }
         
         sendBtn.snp.makeConstraints {
             $0.trailing.equalTo(snp.trailing).offset(-10)
             $0.centerY.equalToSuperview()
             $0.height.equalTo(26)
+            $0.width.equalTo(35)
         }
         
         stackView.snp.makeConstraints {
             $0.leading.equalTo(snp.leading).offset(10)
             $0.trailing.equalTo(snp.trailing).offset(-10)
             $0.height.equalTo(26)
+            $0.height.greaterThanOrEqualTo(50)
             $0.centerY.equalToSuperview()
         }
         
