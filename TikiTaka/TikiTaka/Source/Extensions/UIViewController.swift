1f3e365aaf0141e8ad43f83534320d6c5e5d1911
Ga-yo <39812@naver.com>
2021-03-17 19:27:56 +0900

    [Add] extension UIApplication

--- TikiTaka/TikiTaka/Source/Extensions/UIViewController.swift
+++ TikiTaka/TikiTaka/Source/Extensions/UIViewController.swift
@@ -37,14 +37,31 @@ extension UIViewController {
 
 extension BehaviorRelay where Element: RangeReplaceableCollection {
     func add(element: Element.Element) {
-        var array = self.value
+        var array = value
         array.append(element)
-        self.accept(array)
+        accept(array)
     }
-    
-    func insert(element: Element.Element) {
-        var array = self.value
-        array.insert(element, at: 0 as! Element.Index)
-        self.accept(array)
+}
+
+extension UIApplication {
+    var statusBarUIView: UIView? {
+        if #available(iOS 13.0, *) {
+            let tag = 38482
+            let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+            
+            if let statusBar = keyWindow?.viewWithTag(tag) {
+                return statusBar
+            } else {
+                guard let statusBarFrame = keyWindow?.windowScene?.statusBarManager?.statusBarFrame else { return nil }
+                let statusBarView = UIView(frame: statusBarFrame)
+                statusBarView.tag = tag
+                keyWindow?.addSubview(statusBarView)
+                return statusBarView
+            }
+        } else if responds(to: Selector(("statusBar"))) {
+            return value(forKey: "statusBar") as? UIView
+        } else {
+            return nil
+        }
     }
 }
